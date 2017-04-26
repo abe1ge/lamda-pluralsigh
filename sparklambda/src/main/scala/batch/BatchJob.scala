adc91b434f6864c1cdf88af6be9468a385bf85ed
abe1ge <greenfire@live.co.uk>
2017-04-26 22:42:35 +0100

    3.5 Aggregations with DataFram... running some quirys on the data

--- sparklambda/src/main/scala/batch/BatchJob.scala
+++ sparklambda/src/main/scala/batch/BatchJob.scala
@@ -2,6 +2,7 @@ package batch
 
 import java.lang.management.ManagementFactory
 import org.apache.spark.{SparkContext, SparkConf}
+import domain._
 /**
   * Created by work on 26/04/2017.
   */
@@ -25,7 +26,32 @@ object BatchJob {
     val sourceFile = "file:///C:/Users/work/Documents/plursight/applyingLambdaArchitecture/Boxes/spark-kafka-cassandra-applying-lambda-architecture/vagrant/data.tsv"
     val input = sc.textFile(sourceFile)
 
-    // Spark action results in job execution
-    input.foreach(println)
+    val inputRDD = input.flatMap{ line =>
+      val record = line.split("\\t")
+      val MS_IN_HOUR = 1000 * 60 * 60
+      if (record.length == 7)
+      Some(Activity(record(0).toLong / MS_IN_HOUR * MS_IN_HOUR, record(1) , record(2), record(3), record(4),record(5),record(6)))
+      else
+        None
+    }
+
+    val keyedByProduct = inputRDD.keyBy(x => (x.product, x.timestamp_hour)).cache()
+
+    val visitorsByProduct = keyedByProduct
+      .mapValues( x => x.visitor)
+      .distinct()
+      .countByKey()
+
+    val activityByProduct = keyedByProduct
+      .mapValues( x =>
+      x.action match {
+        case "purchase" => (1, 0, 0)
+        case "add_to_cart" => (0, 1, 0)
+        case "page_view" => (0, 0, 1)
+      })
+      .reduceByKey((a, b) => (a._1 + b._1,a._2 + b._2, a._3 + b._3))
+
+    visitorsByProduct.foreach(println)
+    activityByProduct.foreach(println)
   }
 }
---
+++ sparklambda/src/main/scala/domain/package.scala
@@ -0,0 +1,14 @@
+/**
+  * Created by work on 26/04/2017.
+  */
+package object domain {
+  case class Activity(timestamp_hour: Long,
+                      referrer: String,
+                      action: String,
+                      prevPage: String,
+                      page: String,
+                      visitor: String,
+                      product: String,
+                      inputProps: Map[String, String] = Map()
+                     )
+}
