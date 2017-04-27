07ec038c09e75a23fe47dd69266a2002fefc88cf
abe1ge <greenfire@live.co.uk>
2017-04-27 21:58:30 +0100

    3.6 Aggregations with DataFram

--- sparklambda/src/main/scala/batch/BatchJob.scala
+++ sparklambda/src/main/scala/batch/BatchJob.scala
@@ -2,6 +2,7 @@ package batch
 
 import java.lang.management.ManagementFactory
 import org.apache.spark.{SparkContext, SparkConf}
+import org.apache.spark.sql.SQLContext
 import domain._
 /**
   * Created by work on 26/04/2017.
@@ -21,36 +22,60 @@ object BatchJob {
 
     // setup spark
     val sc = new SparkContext(conf)
+    implicit val sqlContext = new SQLContext(sc)
+
+    import org.apache.spark.sql.functions._
+    import sqlContext.implicits._
+
 
     // initialize input RDD
     val sourceFile = "file:///C:/Users/work/Documents/plursight/applyingLambdaArchitecture/Boxes/spark-kafka-cassandra-applying-lambda-architecture/vagrant/data.tsv"
     val input = sc.textFile(sourceFile)
 
-    val inputRDD = input.flatMap{ line =>
+    val inputDF = input.flatMap{ line =>
       val record = line.split("\\t")
       val MS_IN_HOUR = 1000 * 60 * 60
       if (record.length == 7)
       Some(Activity(record(0).toLong / MS_IN_HOUR * MS_IN_HOUR, record(1) , record(2), record(3), record(4),record(5),record(6)))
       else
         None
-    }
-    
+    }.toDF()
 
-    val keyedByProduct = inputRDD.keyBy(x => (x.product, x.timestamp_hour)).cache()
+    sqlContext.udf.register("UnderExposed", (pageViewCount: Long, purchaseCount: Long) => if (purchaseCount == 0) 0 else pageViewCount / purchaseCount)
 
-    val visitorsByProduct = keyedByProduct
-      .mapValues( x => x.visitor)
-      .distinct()
-      .countByKey()
+    val df = inputDF.select(
+      add_months(from_unixtime(inputDF("timestamp_hour") / 1000), 1).as("timestamp_hour"),
+      inputDF("referrer"), inputDF("action"), inputDF("prevPage"), inputDF("page"), inputDF("visitor"), inputDF("product"))
+      .cache()
 
-    val activityByProduct = keyedByProduct
-      .mapValues( x =>
-      x.action match {
-        case "purchase" => (1, 0, 0)
-        case "add_to_cart" => (0, 1, 0)
-        case "page_view" => (0, 0, 1)
-      })
-      .reduceByKey((a, b) => (a._1 + b._1,a._2 + b._2, a._3 + b._3))
+    df.registerTempTable("activity")
+
+    val visitorsByProduct = sqlContext.sql(
+      """SELECT product, timestamp_hour, COUNT(DISTINCT visitor) as unique_visitors
+        |FROM activity GROUP BY product, timestamp_hour
+      """.stripMargin)
+
+    visitorsByProduct.printSchema()
+
+    val activityByProduct = sqlContext.sql(
+      """SELECT product, timestamp_hour,
+        |sum(case when action = 'purchase' then 1 else 0 end) as purchase_count,
+        |sum(case when action = 'add_to_cart' then 1 else 0 end) as add_to_cart_count,
+        |sum(case when action = 'page_view' then 1 else 0 end) as page_view_count
+        |FROM activity
+        |GROUP BY product, timestamp_hour
+      """.stripMargin)
+
+    activityByProduct.registerTempTable("activityByProduct")
+
+    val underExposedProducts = sqlContext.sql(
+      """
+        |SELECT product, timestamp_hour,
+        |UnderExposed(page_view_count, purchase_count) as negative_exposure
+        |FROM activityByProduct
+        |ORDER BY negative_exposure DESC
+        |limit 5
+      """.stripMargin)
 
     visitorsByProduct.foreach(println)
     activityByProduct.foreach(println)
